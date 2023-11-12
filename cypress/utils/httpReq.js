1d750eb3f04b3c802eb62e7692887574fb6611b0
VictorUdod <victoroudod@gmail.com>
2023-11-12 23:04:54 +0300

    API req file

--- cypress/utils/httpReq.js
+++ cypress/utils/httpReq.js
@@ -1,42 +1,4 @@
 
-// export function httpReq(method, apiUrl, statusCode, petData = null){
-    
-
-//     let reqConfig = {
-//         method: method,
-//         url: apiUrl,
-//         failOnStatusCode: false
-//     };
-
-//     if(method === 'POST'){
-//         reqConfig.body = petData;
-//         cy.request(reqConfig).then((res) => {
-
-//             console.log('Actual Response Body:', res.body);
-//             console.log('Expected Pet Data:', petData);
-    
-//             expect(res.status).to.eq(statusCode);
-//             if(statusCode === 200){
-//             expect(res.body).to.deep.equal(petData)}
-//         });
-//     } else if (method === 'DELETE'){
-//         cy.request(reqConfig).then((res) => {
-//             expect(res.status).to.eq(statusCode);
-//             expect(res.body).to.have.property('message', petData)
-//         })
-//     } 
-//     else if(method === 'GET'){
-//         cy.request(reqConfig).then((res) => {
-//             expect(res.status).to.eq(statusCode);
-//             if(statusCode === 200){
-//             expect(res.body).to.deep.equal(petData)
-//             }
-//         })
-//     } 
-
-    
-// }
-
 export function httpReq(method, apiUrl, statusCode, data = null) {
     const reqConfig = {
         method,
